45bad18c0d0e6baf5b05f2aaad79bd88a8672fb1
Aloxan12 <kvakvaaak@mail.ru>
2022-12-08 14:22:50 +0300

    update users with Mongoose

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req:Request, res:any) => {
     let message = 'Hello World!!2!'
     res.send(message)
 })
