45bafa6940d5ad354f172744e175ba1eb47e8bed
Aloxan12 <kvakvaaak@mail.ru>
2022-12-08 14:33:58 +0300

    update users with Mongoose

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 "use strict";
-const express = require('express');
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
     let message = 'Hello World!! !!';
