972c52ac8fcd2847d5b3131f94be2e6bfbae4f90
Aloxan12 <kvakvaaak@mail.ru>
2022-12-08 14:18:08 +0300

    update users with Mongoose

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    let message = 'Hello World!!2!'
+    res.send(message)
 })
 
 app.listen(port, () => {
