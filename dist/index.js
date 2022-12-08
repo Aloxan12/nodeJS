2f77fd74dcba0493f7a2e6ac92330ad48281457f
Aloxan12 <kvakvaaak@mail.ru>
2022-12-08 14:27:28 +0300

    update users with Mongoose

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    let message = 'Hello World!!!';
+    res.send(message);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req:Request, res:any) => {
-    let message = 'Hello World!!2!'
+    let message = 'Hello World!!!'
     res.send(message)
 })
 
