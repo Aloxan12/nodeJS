67c559f3964dd4bbb82ad5c5a6df5d45188450f2
Aloxan12 <kvakvaaak@mail.ru>
2022-12-11 10:25:44 +0300

    init

--- dist/index.js
+++ dist/index.js
@@ -10,17 +10,31 @@ app.get('/', (req, res) => {
     let message = 'Hello World!!!!';
     res.send(message);
 });
-const products = [{ title: 'bread' }, { title: 'apple' }, { title: 'orange' }];
-const addresses = [{ value: 'Minsk' }, { value: 'London' }, { value: 'Grodno' }];
+const products = [{ id: 1, title: 'bread' }, { id: 2, title: 'apple' }, { id: 3, title: 'orange' }];
+const addresses = [{ id: 1, value: 'Minsk' }, { id: 2, value: 'London' }, { id: 3, value: 'Grodno' }];
 app.get('/products', (req, res) => {
     res.send(products);
 });
 app.get('/products/:productTitle', (req, res) => {
     let product = products.find((prod) => prod.title === req.params.productTitle);
-    res.send(product);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.get('/addresses', (req, res) => {
     res.send(addresses);
+});
+app.get('/addresses/:id', (req, res) => {
+    let address = addresses.find((address) => address.id === +req.params.id);
+    if (address) {
+        res.send(address);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ app.get('/', (req:Request, res:Response) => {
     res.send(message)
 })
 
-const products = [{title:'bread'}, {title:'apple'}, {title:'orange'}]
-const addresses = [{value:'Minsk'}, {value:'London'}, {value:'Grodno'}]
+const products = [{id: 1, title:'bread'}, {id: 2,title:'apple'}, {id: 3, title:'orange'}]
+const addresses = [{id: 1, value:'Minsk'}, {id: 2,value:'London'}, {id: 3,value:'Grodno'}]
 
 app.get('/products', (req:Request, res:Response) => {
     res.send(products)
@@ -17,12 +17,26 @@ app.get('/products', (req:Request, res:Response) => {
 
 app.get('/products/:productTitle', (req:Request, res:Response) => {
     let product = products.find((prod)=> prod.title === req.params.productTitle)
-    res.send(product)
+    if(product) {
+        res.send(product)
+    }else {
+        res.send(404)
+    }
 })
 
 app.get('/addresses', (req:Request, res:Response) => {
     res.send(addresses)
 })
+
+app.get('/addresses/:id', (req:Request, res:Response) => {
+    let address = addresses.find((address)=> address.id === +req.params.id)
+    if(address) {
+        res.send(address)
+    }else {
+        res.send(404)
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
