2268dba2a06d871a5f7ee020210516717de97ba9
Aloxan12 <kvakvaaak@mail.ru>
2022-12-11 10:20:55 +0300

    init

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,18 @@ app.get('/', (req, res) => {
     let message = 'Hello World!!!!';
     res.send(message);
 });
+const products = [{ title: 'bread' }, { title: 'apple' }, { title: 'orange' }];
+const addresses = [{ value: 'Minsk' }, { value: 'London' }, { value: 'Grodno' }];
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+app.get('/products/:productTitle', (req, res) => {
+    let product = products.find((prod) => prod.title === req.params.productTitle);
+    res.send(product);
+});
+app.get('/addresses', (req, res) => {
+    res.send(addresses);
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,22 @@ app.get('/', (req:Request, res:Response) => {
     res.send(message)
 })
 
+const products = [{title:'bread'}, {title:'apple'}, {title:'orange'}]
+const addresses = [{value:'Minsk'}, {value:'London'}, {value:'Grodno'}]
+
+app.get('/products', (req:Request, res:Response) => {
+    res.send(products)
+})
+
+app.get('/products/:productTitle', (req:Request, res:Response) => {
+    let product = products.find((prod)=> prod.title === req.params.productTitle)
+    res.send(product)
+})
+
+app.get('/addresses', (req:Request, res:Response) => {
+    res.send(addresses)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
