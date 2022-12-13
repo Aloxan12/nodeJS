3bdf7d3df536077379bbce621e2078096a9d4113
Aloxan12 <kvakvaaak@mail.ru>
2022-12-13 09:56:38 +0300

    init

--- src/respositories/products-respository.ts
+++ src/respositories/products-respository.ts
@@ -19,5 +19,26 @@ export const productsRespository = {
             title: title}
         products.push(newProduct)
         return newProduct
+    },
+    updateProduct(id: number, title: string){
+        let product = products.find((prod)=> prod.id === id)
+        if(product) {
+            product.title = title
+            return product
+        }else {
+            return null
+        }
+    },
+    deleteProduct(id: number){
+        for(let i = 0; i < products.length; i++){           //#1
+            if(products[i].id === id){
+                products.splice(i, 1);
+                return true;
+            }
+        }
+        return false
+        
+        // const newPropucts = products.filter((prod)=> prod.id !== +req.params.id) //#2 придумать как вернуть с кодом
+        // return newPropucts.length < products.length ? res.send(201) : res.send(404)
     }
 }
--- src/routers/product-router.ts
+++ src/routers/product-router.ts
@@ -24,24 +24,14 @@ productRouter.post('/', (req:Request, res:Response) => {
     res.status(201).send(newProduct)
 })
 productRouter.put('/:id', (req:Request, res:Response) => {
-    let product = products.find((prod)=> prod.id === +req.params.id)
+    let product = productsRespository.updateProduct(+req.params.id, req.body.title)
     if(product) {
-        product.title = req.body.title
         res.send(product)
     }else {
         res.send(404)
     }
 })
 productRouter.delete('/:id', (req:Request, res:Response) => {
-    for(let i = 0; i < products.length; i++){           //#1
-        if(products[i].id === +req.params.id){
-            products.splice(i, 1);
-            res.send(201);
-            return;
-        }
-    }
-    return res.send(404)
-
-    // const newPropucts = products.filter((prod)=> prod.id !== +req.params.id) //#2 придумать как вернуть с кодом
-    // return newPropucts.length < products.length ? res.send(201) : res.send(404)
+    const isDeleted = productsRespository.deleteProduct(+req.params.id)
+    return isDeleted ? res.send(204) : res.send(404)
 })
