ad74687d05367a497f0657b2df118694ad187f36
Aloxan12 <kvakvaaak@mail.ru>
2022-12-13 09:14:58 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express, {Request, Response} from 'express'
 import bodyParser from 'body-parser'
 import { productRouter } from './routers/product-router'
+import { addressesRouter } from './routers/addresses-router'
 
 const app = express()
 
 
 const port = process.env.PORT || 3005
-
-const addresses = [{id: 1, value:'Minsk'}, {id: 2,value:'London'}, {id: 3,value:'Grodno'}]
 
 const parserMiddleweare = bodyParser()
 app.use(parserMiddleweare)
@@ -17,21 +16,8 @@ app.get('/', (req:Request, res:Response) => {
     res.send(message)
 })
 
-
-app.get('/addresses', (req:Request, res:Response) => {
-    res.send(addresses)
-})
-
-app.get('/addresses/:id', (req:Request, res:Response) => {
-    let address = addresses.find((address)=> address.id === +req.params.id)
-    if(address) {
-        res.send(address)
-    }else {
-        res.send(404)
-    }
-})
-
 app.use('products', productRouter)
+app.use('addresses', addressesRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
