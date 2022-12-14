660143cabb6904654d8d537097a2e74fc3de3740
Aloxan12 <kvakvaaak@mail.ru>
2022-12-14 13:21:50 +0300

    init

--- src/routers/product-router.ts
+++ src/routers/product-router.ts
@@ -1,11 +1,15 @@
 import {Request, Response} from 'express'
 import { Router } from "express";
+import { body } from 'express-validator';
+import { inputValidationMiddleware } from '../middlewares/input-validation-middleware';
 import { productsRespository } from '../respositories/products-respository';
 
 const products = [{id: 1, title:'bread'}, {id: 2,title:'apple'}, {id: 3, title:'orange'}]
 
 
 export const productRouter = Router({})
+
+const titleValidation = body('title').trim().isLength({min: 3, max: 6}).withMessage('Название должно содержать от 3 до 10 символов')
 
 productRouter.get('/', (req:Request, res:Response) => {
     const foundProducts = productsRespository.findProduct(req.query.title?.toString())
@@ -19,11 +23,11 @@ productRouter.get('/:id', (req:Request, res:Response) => {
         res.send(404)
     }
 })
-productRouter.post('/', (req:Request, res:Response) => {
+productRouter.post('/',titleValidation,inputValidationMiddleware , (req:Request, res:Response) => {
     const newProduct = productsRespository.createProduct(req.body.title)
     res.status(201).send(newProduct)
 })
-productRouter.put('/:id', (req:Request, res:Response) => {
+productRouter.put('/:id',titleValidation,inputValidationMiddleware , (req:Request, res:Response) => {
     let product = productsRespository.updateProduct(+req.params.id, req.body.title)
     if(product) {
         res.send(product)
