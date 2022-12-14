678dc83c02fae5e584a1a428cc09b21cea7f541b
Aloxan12 <kvakvaaak@mail.ru>
2022-12-14 13:18:16 +0300

    init

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -1,3 +1,11 @@
 import { NextFunction, Response, Request } from "express";
+import { validationResult } from "express-validator";
 
-export const inputValidationMiddleware = (req:Request, res: Response, next:NextFunction)=>{}
+export const inputValidationMiddleware = (req:Request, res: Response, next:NextFunction)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        res.status(400).json({errors: errors.array()})
+    }else {
+        next()
+    }
+}
