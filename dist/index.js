e73cea824a4792abe5597e526fcc139ef3366bd3
Aloxan12 <kvakvaaak@mail.ru>
2022-12-15 13:35:53 +0300

    async

--- dist/index.js
+++ dist/index.js
@@ -13,20 +13,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const product_router_1 = require("./routers/product-router");
 const addresses_router_1 = require("./routers/addresses-router");
 const db_1 = require("./respositories/db");
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3005;
-const parserMiddleweare = body_parser_1.default.json();
+const port = process.env.PORT || 5000;
+const parserMiddleweare = express_1.default.json();
 app.use(parserMiddleweare);
+app.get('/', (req, res) => {
+    res.send('Hello Samurai');
+});
 app.use('products', product_router_1.productRouter);
 app.use('addresses', addresses_router_1.addressesRouter);
 const startApp = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield (0, db_1.runDb)();
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);
-    });
+    try {
+        yield (0, db_1.runDb)();
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
+    }
+    catch (_a) {
+        console.log('error');
+    }
 });
 startApp();
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import express, {Request, Response} from 'express'
-import bodyParser from 'body-parser'
 import { productRouter } from './routers/product-router'
 import { addressesRouter } from './routers/addresses-router'
 import { runDb } from './respositories/db'
 
 const app = express()
+const port = process.env.PORT || 5000
 
-
-const port = process.env.PORT || 3005
-
-const parserMiddleweare = bodyParser.json()
+const parserMiddleweare = express.json()
 app.use(parserMiddleweare)
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello Samurai')
+})
 
 app.use('products', productRouter)
 app.use('addresses', addressesRouter)
