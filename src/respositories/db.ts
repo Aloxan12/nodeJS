c55d2aa066c8a33c699c61a14eb9e49fc1478d0f
Aloxan12 <kvakvaaak@mail.ru>
2022-12-18 10:06:49 +0300

    async

--- src/respositories/db.ts
+++ src/respositories/db.ts
@@ -1,24 +1,16 @@
-import {MongoClient} from 'mongodb'
 import * as dotenv from 'dotenv'
+import * as firebase from 'firebase/app'
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+const credential = require('../../nodejs-a451c-firebase-adminsdk-cw6uj-39f389fd44.json')
+
 dotenv.config()
 
-const mongoUri = process.env.mongoURI || 'mongodb://0.0.0.0:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
 
-// Connection URL
-const url = process.env.MONGO_URL
-console.log('url :', url)
-if (!mongoUri) {
-    throw new Error('❗ Url doesn\'t found')
-}
-// const client = new MongoClient(url);
-
-export const client = new MongoClient(mongoUri)
 export async function runDb(){
     try{
-        await client.connect()
-        await client.db('products')
+        const app = firebase.initializeApp(credential)
+        const db = getFirestore(app)
         console.log('Connected successfully to mongo server')
     }catch{
-        await client.close()
     }
 }
