0ceb217b3987caedff266d4f3af3ae54f9369682
Aloxan12 <kvakvaaak@mail.ru>
2022-12-25 11:12:24 +0300

    db

--- src/respositories/token-repository.ts
+++ src/respositories/token-repository.ts
@@ -1,11 +1,13 @@
 import { tokenModel } from "../models/token-model"
+import * as dotenv from "dotenv";
+import jwt  from 'jsonwebtoken'
 
-const jwt = require('jsonwebtoken')
+dotenv.config()
 
 export const tokenRepository = {
     generateTokens(payload: any){
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30m'})
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET as string, {expiresIn: '30m'})
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET as string, {expiresIn: '30d'})
 
         return {
             accessToken,
@@ -14,7 +16,7 @@ export const tokenRepository = {
     },
     validateAccessToken(token: string){
         try {
-            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET  as string)
             return userData
         }catch (e) {
             return null
@@ -22,7 +24,7 @@ export const tokenRepository = {
     },
     validateRefreshToken(token: string){
         try {
-            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET  as string)
             return userData
         }catch (e) {
             return null
