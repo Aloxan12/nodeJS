0e7ef8cc30ff0ec50f33fd6f5ff7a3f079ed6dab
Aloxan12 <kvakvaaak@mail.ru>
2023-02-02 04:10:16 +0300

    file upload

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -9,4 +9,5 @@ export const authRouter = Router({})
 
 authRouter.post('/registration', userController.registration)
 authRouter.post('/login', userController.login)
+authRouter.post('/logout', userController.logout)
 authRouter.get('/refresh', userController.refresh)
