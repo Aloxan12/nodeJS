e6e0a633b817a6211dacfd2203f399f1da149eeb
Aloxan12 <kvakvaaak@mail.ru>
2023-11-27 16:42:19 +0300

    file upload

--- src/respositories/google-drive.ts
+++ src/respositories/google-drive.ts
@@ -3,11 +3,11 @@ import path from "path"
 import {google} from 'googleapis'
 
 
-const KEYFILEPATH = path.join(__dirname, "credentials.json");
+const KEYFILEPATH = path.join(__dirname,'..', '..', "credentials.json");
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILEPATH,
+    keyFile: process.env.credentials,
     scopes: SCOPES,
 });
 
