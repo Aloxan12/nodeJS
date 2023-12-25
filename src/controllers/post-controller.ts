10054ee0f562046f7b2b84697ca0c537ebe21954
Aloxan12 <kvakvaaak@mail.ru>
2023-12-25 07:29:09 +0300

    file upload

--- src/controllers/post-controller.ts
+++ src/controllers/post-controller.ts
@@ -1,11 +1,4 @@
 import {Request, Response, NextFunction} from 'express'
-import {validationResult} from 'express-validator'
-import {ApiError} from '../exeptions/api-error'
-import {UserModel} from '../models/user-model';
-import {userRespository} from '../respositories/user-respository';
-import path from "path";
-import fs from 'fs' ;
-import {UploadedFile} from 'express-fileupload';
 import {postRespository} from '../respositories/post-respository';
 
 export const postController = {
--- src/respositories/post-respository.ts
+++ src/respositories/post-respository.ts
@@ -1,13 +1,6 @@
 import {ApiError} from "../exeptions/api-error"
-import {UserModel} from "../models/user-model"
-import bcrypt from "bcrypt"
-import {productsCollection, ProductType} from "./dbMongo"
-import {mailRepository} from "./mail-repository"
-import {tokenRepository} from "./token-repository"
-import {IUserDto, RoleType, UserDto, IUserDtoBD} from "../dtos/user-dto"
-import mongodb from 'mongodb'
 import { PostModel } from "../models/post-model"
-import { IPostDto, IPostDtoBD, PostDto } from "../dtos/post-dto"
+import { IPostDtoBD, PostDto } from "../dtos/post-dto"
 
 export const postRespository = {
     async getAllPosts(search: string, limit: string | number, page: string | number, userId: string){
