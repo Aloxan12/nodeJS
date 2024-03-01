f46489157153db417803d0859a41cfe156c38f1c
Aloxan12 <kvakvaaak@mail.ru>
2024-03-01 12:31:35 +0300

    post upload

--- src/websockets/websocket-server.ts
+++ src/websockets/websocket-server.ts
@@ -4,10 +4,10 @@ export const setupSocketServer = (server: any) => {
     const io = new Server(server, {
         cors: {
             credentials: true,
-            origin: ['http://localhost:3000', 'https://aloxan12.github.io'],
+            origin: ['http://localhost', 'http://localhost:3000', 'https://aloxan12.github.io'],
             methods: ["GET", "POST"],
         },
-        pingTimeout: 60000,
+        transports: ['websocket', 'polling'],
         allowEIO3: true
     });
 
