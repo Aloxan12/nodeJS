5be62052b2d632957194849402e35d7479518bfc
Aloxan12 <kvakvaaak@mail.ru>
2024-03-01 12:02:34 +0300

    post upload

--- src/websockets/websocket-server.ts
+++ src/websockets/websocket-server.ts
@@ -1,7 +1,13 @@
 import { Server } from 'socket.io';
 
 export const setupSocketServer = (server: any) => {
-    const io = new Server(server);
+    const io = new Server(server, {
+        cors: {
+            credentials: true,
+            origin: ['http://localhost:3000', 'https://aloxan12.github.io'],
+            methods: ["GET", "POST"]
+        }
+    });
 
     io.on('connection', (socket) => {
         console.log('a user connected');
