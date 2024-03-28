784535c0c0023ab9c493c9e0e3ff8f751442db33
Aloxan12 <kvakvaaak@mail.ru>
2024-03-28 16:46:27 +0300

    message model

--- src/respositories/chat-respository.ts
+++ src/respositories/chat-respository.ts
@@ -1,12 +1,14 @@
-import { IPostDtoBD } from "../dtos/post-dto"
 import { ChatModel } from "../models/chat-model";
+import {IChatDtoBD} from "../dtos/chat-dto";
+import {ApiError} from "../exeptions/api-error";
+import {PostModel} from "../models/post-model";
 
 export const chatRepository = {
-    async getAllChats(search: string, limit: string | number, page: string | number, userId: string){
+    async getAllChats(search: string, limit: string | number, page: string | number){
         page = Number(page || 1)
         limit = Number(limit || 5)
         let offset = Number(page * limit - limit)
-        const chats: IPostDtoBD[] = await ChatModel.find().populate('author').populate('likes');
+        const chats: IChatDtoBD[] = await ChatModel.find().populate('users');
 
         const totalCount = chats.length;
         const totalPages = Math.ceil(totalCount / limit);
@@ -20,5 +22,18 @@ export const chatRepository = {
             count: totalCount,
             results: chats.slice(offset, offset + limit)
         };
+    },
+
+    async createChat(postText: string, author: number,){
+
+        const post = await ChatModel.create({postText, author, publicDate: new Date(), likes:[]})
+        await post.populate({
+            path: 'author',
+            select: '-password -__v -activationLink',
+        });
+        const postDto = {...post,isLike: false, likeCount: 0  }
+        return {
+            post: postDto
+        }
     },
 }
