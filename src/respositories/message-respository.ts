4f45226685c25802f797d0ecacd51a7e0b063fd2
Aloxan12 <kvakvaaak@mail.ru>
2024-04-11 21:08:33 +0300

    message model

--- src/respositories/message-respository.ts
+++ src/respositories/message-respository.ts
@@ -3,11 +3,22 @@ import {formatMessage} from "./helpers/messageHelpers";
 import {IMessageDtoBD} from "../dtos/messages-dto";
 import {ObjectId} from "mongodb";
 
+interface QueryParams{
+    [key: string]: string | null
+}
+
 export const messageRepository = {
     async getAllMessages(userId: string, search: string, limit: string | number, page: string | number, chatId?: string){
         page = Number(page || 1)
         limit = Number(limit || 5)
         let offset = Number(page * limit - limit)
+
+        let query: QueryParams = { author: userId }
+        if (chatId) {
+            query = { ...query, chatId: chatId }
+        } else {
+            query = { ...query, chatId: null }
+        }
         // Фильтрация чатов по пользователю userId
         const messages: IMessageDtoBD[] = await MessageModel.find({ 'chatId': { $elemMatch: { $eq: new ObjectId(chatId) } } }).populate('author')
 
