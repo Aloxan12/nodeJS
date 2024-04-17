c0b0b4b098c14b9f801aaf35ee3fcbf96e0b7493
Aloxan12 <kvakvaaak@mail.ru>
2024-04-17 14:44:34 +0300

    message model

--- src/websockets/websocket-server.ts
+++ src/websockets/websocket-server.ts
@@ -10,27 +10,15 @@ interface MessageType {
     chatId: string
 }
 
-
-const message = {
-    id: 1,
-    event: 'greeting',
-    user: {
-        name: ''
-    },
-    text: 'Добро пожаловать в чат',
-};
-
 export const setupWebSocketServer = (httpServer: any) => {
     const wsServer = new WebSocketServer({ server: httpServer });
 
     wsServer.on('connection', ws => {
-        ws.send(JSON.stringify(message))
         ws.on('close', () => console.log('Client has disconnected!'))
 
         ws.on('message',  async (message) => {
             const messageObj: MessageType = JSON.parse(message.toString())
             if(messageObj.event === 'message'){
-                console.log('meme')
                 await messageRepository.createMessage(messageObj.author?.id, messageObj.text, messageObj.chatId)
             }
             wsServer.clients.forEach(client => {
