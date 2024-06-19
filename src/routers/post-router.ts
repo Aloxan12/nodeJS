c732d406feb49885afbf431309ffadd5317be0c5
Aloxan12 <kvakvaaak@mail.ru>
2024-06-19 16:26:25 +0300

    message model

--- src/routers/post-router.ts
+++ src/routers/post-router.ts
@@ -1,15 +1,9 @@
 import {Router} from "express";
-import {body} from 'express-validator';
 import {postController} from '../controllers/post-controller';
 import {authMiddleware} from '../middlewares/auth-middleware';
 
 
 export const postRouter = Router({})
-
-const titleValidation = body('title').trim().isLength({
-    min: 3,
-    max: 10
-}).withMessage('Название должно содержать от 3 до 10 символов')
 
 postRouter.get('/', authMiddleware, postController.getPosts)
 postRouter.post('/', authMiddleware, postController.createPosts)
